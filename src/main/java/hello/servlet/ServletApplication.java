786ac7a13d310b6badffa3028ccb4730f2aeeeb6
02ggang9 <02ggang9@gmail.com>
2024-01-06 17:33:17 +0900

    feat: Hello 서블릿

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 자동 서블릿 등록 (동등, 하위 패키지를 다 뒤짐)
 @SpringBootApplication
 public class ServletApplication {
 
